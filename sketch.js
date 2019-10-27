c9808d55e0d5c085c69430d48630b23d9a3dc566
Yevhenii Kubiuk <yevhenii.kubiuk@gmail.com>
2019-10-27 15:11:39 +0200

    changed ops shape-based approach to pixel-based

--- sketch.js
+++ sketch.js
@@ -22,37 +22,8 @@ class Line {
         this.end = v2;
     }
 
-    intersection(l) {
-        const bx = this.end.x - this.start.x;
-        const by = this.end.y - this.start.y;
-        const dx = l.end.x - l.start.x;
-        const dy = l.end.y - l.start.y;
-
-        const b_dot_d_perp = bx * dy - by * dx;
-
-        if (b_dot_d_perp === 0) {
-            return null;
-        }
-
-        const cx = l.start.x - this.start.x;
-        const cy = l.start.y - this.start.y;
-
-        const t = (cx * dy - cy * dx) / b_dot_d_perp;
-
-        if (t < 0 || t > 1) {
-            return null;
-        }
-
-        const u = (cx * by - cy * bx) / b_dot_d_perp;
-        if (u < 0 || u > 1) {
-            return null;
-        }
-
-        return createVector(this.start.x + t * bx, this.start.y + t * by);
-    }
-
-    show() {
-        stroke(0);
+    show(color = 0) {
+        stroke(color);
         strokeWeight(1);
         line(this.start.x, this.start.y, this.end.x, this.end.y);
     }
@@ -60,141 +31,147 @@ class Line {
 
 class set {
     constructor(a, b, c) {
-        this.a = a;
-        this.b = b;
-        this.c = c;
-
         this.v1 = v(a, 0.0);
         this.v2 = v(b, FH);
         this.v3 = v(c, 0.0);
 
-        this.l1 = new Line(this.v1, this.v2);
-        this.l2 = new Line(this.v2, this.v3);
+        this.shape = new shape([this.v1, this.v2, this.v3]);
     }
-
-    show() {
-        this.l1.show();
-        this.l2.show();
-    }
-
 }
 
 class shape {
-    constructor(labels, vertices) {
-        this.labels = labels;
+    constructor(vertices) {
         this.vertices = vertices;
     }
+
+    show() {
+        beginShape();
+        strokeWeight(1);
+        fill('rgba(255,0,0, 0.3)');
+        this.vertices.forEach(v => {
+            vertex(v.x, v.y);
+        });
+        endShape();
+    }
+
+    inside(point) {
+        const x = point.x, y = point.y;
+        const vs = this.vertices;
+        let inside = false;
+
+        for (let i = 0, j = vs.length - 1; i < vs.length; j = i++) {
+            const xi = vs[i].x, yi = vs[i].y;
+            const xj = vs[j].x, yj = vs[j].y;
+            
+            const intersect = ((yi > y) != (yj > y))
+                && (x < (xj - xi) * (y - yi) / (yj - yi) + xi);
+            if (intersect) inside = !inside;
+        }
+        
+        return inside;
+    };
 }
 
 class fis {
     constructor(sets) {
         this.sets = sets;
-        this.int = [];
-        this.sb = {};
-        this.selection = {};
-
-        for (let i = 0; i < sets.length - 1; i++) {
-            this.int.push(sets[i].l2.intersection(sets[i + 1].l1));
-        }
-
-        this._initShapeBuffer();
-    }
-
-    _initShapeBuffer() {
-        this.sb[k(0)] = new shape([0], [
-            this.sets[0].v1,
-            this.sets[0].v2,
-            this.int[0],
-            this.sets[1].v1,
-            this.sets[0].v1
-        ]
-        );
-
-        const li = this.sets.length - 1;
-
-        this.sb[k(li)] = new shape([li], [
-            this.sets[li - 1].v3,
-            this.int[li - 1],
-            this.sets[li].v2,
-            this.sets[li].v3,
-            this.sets[li - 1].v3
-        ]
-        );
-
-        for (let i = 1; i < this.sets.length - 1; i++) {
-
-            this.sb[k(i - 1, i)] = new shape([i - 1, i], [
-                this.sets[i].v1,
-                this.sets[i - 1].v3,
-                this.int[i - 1],
-                this.sets[i].v1
-            ]
-            );
-
-            this.sb[k(i)] = new shape([i], [
-                this.sets[i - 1].v3,
-                this.int[i - 1],
-                this.sets[i].v2,
-                this.int[i],
-                this.sets[i + 1].v1,
-                this.sets[i + 1].v3
-            ]
-            );
-
-            this.sb[k(i, i + 1)] = new shape([i, i + 1], [
-                this.sets[i + 1].v1,
-                this.int[i],
-                this.sets[i].v3,
-                this.sets[i + 1].v1
-            ]
-            );
-        }
-    }
-
-    get(index) {
-        const result = {};
-        for (let key in this.sb) {
-            if (key.indexOf(k(index)) !== -1) {
-                result[key] = this.sb[key];
-            }
-        }
-
-        return result;
-    }
-
-    not(index) {
-        return _.omitBy(this.sb, (val, key) => key.indexOf(k(index)) !== -1);
     }
 
     and(a, b) {
         return _.pick(a, _.keys(b));
     }
 
+    get(index) {
+        const s = this.sets[index].shape;
+        const vs = {};
+        for (let i = FH; i <= 0; i++) {
+            for (let j = 0; j < width; j++) {
+                const cv = v(j, i);
+                if (s.inside(cv)) {
+                    vs[k(j, i)] = cv;
+                }
+            }
+        }
+        return vs;
+    }
+
+    not(index) {
+        const s = this.sets[index].shape;
+        const otherShapes = [];
+        this.sets.forEach((set, i) => {
+            if (i !== index) {
+                otherShapes.push(this.sets[i].shape);
+            }
+        });
+
+        const vs = {};
+        for (let i = FH; i <= 0; i++) {
+            for (let j = 0; j < width; j++) {
+                const cv = v(j, i);
+                if (
+                    !s.inside(cv) &&
+                    _.some(otherShapes, (sh) => sh.inside(cv))
+                ) {
+                    vs[k(j, i)] = cv;
+                }
+            }
+        }
+        return vs;
+    }
+
     or(a, b) {
         return _.assign(a, b);
     }
 
+    /**
+     * cv > 1: con
+     * cv < 1: dil
+     * cv = 1: get
+     */
+    scale(index, cv = 1) {
+        const s = this.sets[index];
+        const d1 = s.v2.x - s.v1.x;
+        const d2 = s.v3.x - s.v2.x;
+        const conShape = new shape([
+            v(s.v2.x - d1 * (1 / cv + 0.01), s.v1.y),
+            s.v2,
+            v(s.v2.x + d2 * (1 / cv + 0.01), s.v3.y),
+            v(s.v2.x - d1 * (1 / cv + 0.01), s.v1.y)
+        ]);
+        
+        const vs = {};
+        for (let i = FH; i <= 0; i++) {
+            for (let j = 0; j < width; j++) {
+                const cv = v(j, i);
+                if (
+                    conShape.inside(cv)
+                ) {
+                    vs[k(j, i)] = cv;
+                }
+            }
+        }
+        return vs;
+    }
+
     show() {
-        this.sets.forEach(s => s.show());
-        this.int.forEach(i => {
-            stroke('red');
-            strokeWeight(10);
-            point(i.x, i.y);
+        stroke(0);
+        strokeWeight(1);
+        noFill();
+        this.sets.forEach(s => {
+            beginShape();
+            s.shape.vertices.forEach(vtx => {
+                vertex(vtx.x, vtx.y);
+            });
+            endShape(CLOSE)
         });
     }
 
-    showSB(src) {
-        for (let key in src) {
-            beginShape();
-            strokeWeight(0);
-            fill('rgba(255,0,0, 0.3)');
-            if (src[key]) {
-                src[key].vertices.forEach(v => {
-                    vertex(v.x, v.y);
-                });
-            }
-            endShape();
-        }
+    showV(vts) {
+        for (let key in vts) {
+            stroke('rgba(0, 0, 255, 0.3)');
+            strokeWeight(1);
+            point(vts[key].x, vts[key].y);
+        };
     }
 }
 
@@ -270,19 +247,24 @@ function draw() {
         f.show();
 
         const c = new Pipe(f)
-            .pipe('get', 1)
+            // .pipe('get', 1)
+            // .pipe()
+            // .pipe('get', 2)
+            // .pipe('and')
+            // .pipe()
+            // .pipe('get', 3)
+            // .pipe('or')
+            .pipe('scale', 0, 0.75)
             .pipe()
-            .pipe('get', 2)
+            .pipe('scale', 1)
             .pipe('and')
-            .pipe()
-            .pipe('get', 3)
-            .pipe('or')
-            .pipe()
-            .pipe('not', 2)
-            .pipe('and')
+            // .pipe()
+            // .pipe('not', 2)
+            // .pipe('and')
             .values();
 
-        f.showSB(c);
+        f.showV(c);
+
         done = true;
     }
 }
